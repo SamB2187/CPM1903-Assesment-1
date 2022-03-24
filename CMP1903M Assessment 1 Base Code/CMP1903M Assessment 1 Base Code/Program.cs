0fb066d3b0e3da72519fd4305adcaac0d9a055e3
Samuel-Harwood <101341494+Samuel-Harwood@users.noreply.github.com>
2022-03-24 19:23:31 +0000

    Add files via upload

---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Program.cs
@@ -0,0 +1,40 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my analyse program. Would you like to analyse a downloaded file or enter some text manualy? Enter 'file' for the first option" +
+            " or 'manual' for the second option.");
+
+            var answer = Console.ReadLine();
+            var program = new Program();
+            answer.ToLower();
+            var input = new Input();
+            switch (answer)
+            {
+                case "manual":
+                    input.ManualTextInput();
+                    break;
+
+                case "file":
+                    input.fileTextInput();
+                    break;
+                default:
+                    Console.WriteLine("Error Occured");
+                    break;
+            }
+        }
+
+        //eg.   public void outputConsole(List<int>)
+
+    }
+}
---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Report
+    {
+
+        public void analyseText(List<int> check)
+        {
+        
+            Console.WriteLine($"Sentences: {check[0]}\n" +
+                  $"Vowels: {check[1]}\n" +
+                  $"Consonants: {check[2]}\n" +
+                  $"Uppercase Letters: {check[3]}\n" +
+                  $"Lowercase Letters: {check[4]}\n");
+                 
+
+            Console.ReadLine();
+        }
+
+    }
+}
