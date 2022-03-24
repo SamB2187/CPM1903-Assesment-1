ba6f88d78b191edcf43b8d2dd87885f442b75c6d
Samuel-Harwood <101341494+Samuel-Harwood@users.noreply.github.com>
2022-03-24 19:22:51 +0000

    Add files via upload

---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Analyse
+    {
+       
+        //Handles the analysis of text
+
+        //Method: analyseText
+
+        //Arguments: string
+        //Returns: list of integers
+        //Calculates and returns an analysis of the text
+        public void analyseText(string input)
+        {
+            List<int> values = new List<int>();
+
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+
+
+
+            //1. Sentences
+            int sentences = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string sentence = input[i].ToString();
+
+                if (".?!".Contains(sentence))
+                {
+                    sentences++;
+                }
+                
+            }
+            values.Add(sentences);
+
+            //2. Vowels
+            int vowels = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                string vowel = input[i].ToString();
+                if ("aeiouAEIOU".Contains(vowel))
+                {
+                    vowels++;
+                }
+                
+            }
+            values.Add(vowels);
+
+            //3. Consonants
+            int consonants = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                string vowel = input[i].ToString();
+                if ("B,C,D,F,G,J,K,L,M,N,P,Q,S,T,V,X,Z,H,R,W,Y".Contains(vowel))
+                {
+                    consonants++;
+                }
+                
+            }
+            values.Add(consonants);
+
+            //4. Upper Case Letters
+            int UpperCaseLetters = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                string UCL = input[i].ToString();
+                if ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".Contains(UCL))
+                {
+                    UpperCaseLetters++;
+                }
+                
+            }
+            values.Add(UpperCaseLetters);
+
+            //5. Lower Case letters
+            int LowerCaseLetters = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+               
+                string LCL = input[i].ToString();
+                if ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".Contains(LCL))
+                {
+                    LowerCaseLetters++;
+                }
+                
+            }
+            values.Add(LowerCaseLetters);
+
+            var report = new Report();
+            report.analyseText(values);
+
+
+
+
+
+
+
+        }
+
+    }   
+ 
+            
+          
+
+}
---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Input.cs
@@ -0,0 +1,45 @@
+﻿namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Input
+    {
+        //Handles the text input for Assessment 1
+
+
+
+        //Method: manualTextInput
+        //Arguments: none
+        //Returns: string
+        //Gets text input from the keyboard
+        public void ManualTextInput()
+        {
+            Console.WriteLine("Please enter the text you would like to be analysed.");
+            var text = Console.ReadLine();
+            var analyse = new Analyse();
+            analyse.analyseText(text);
+        }
+
+        //Method: fileTextInput
+        //Arguments: string (the file path)
+        //Returns: string
+        //Gets text input from a .txt file
+        public void fileTextInput()
+        {
+            Console.WriteLine("What is the name of the file?(P.S There should be an example txt files with the name of Example.txt, Example2.txt, Example3.txt you can test out)");
+            string fileName = Console.ReadLine();
+            if (File.Exists(fileName))
+            {
+                var Text = System.IO.File.ReadAllText(fileName); //You will have to change this to where you place a text file
+                var analyse = new Analyse();
+                analyse.analyseText(Text);
+            }
+            else
+            {
+                Console.WriteLine("\nPath Not found\n");
+
+            }
+
+
+        }
+
+    }
+}
